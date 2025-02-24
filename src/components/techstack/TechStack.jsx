3efab671d61bbec6e03f4c203c0c857f0ffe1308
JiaJun <kongjiajun040103@gmail.com>
2025-02-24 00:06:52 +0000

    added react bit component - circular text

--- src/components/techstack/TechStack.jsx
+++ src/components/techstack/TechStack.jsx
@@ -8,13 +8,16 @@ function TechStack() {
         <div className="mobile-separator">━━━━━━━━━</div>
         <div className="wrapper">
             <img src="/techStack/javascript.svg" alt="" />
+            <img src="/techStack/typescript.svg" alt="" />
             <img src="/techStack/html.svg" alt="" />
             <img src="/techStack/css.svg" alt="" />
             <img src="/techStack/mongodb.svg" alt="" />
             <img src="/techStack/firebase.svg" alt="" />
+            <img src="/techStack/angular.svg" alt="" />
             <img src="/techStack/vuejs.svg" alt="" />
             <img src="/techStack/react.svg" alt="" />
             <img src="/techStack/python.svg" alt="" />
+            <img src="/techStack/c++.svg" alt="" />
             <img src="/techStack/java.svg" alt="" />
         </div>
     </div>
