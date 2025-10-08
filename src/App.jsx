cefd75731c239969c80594a249d23691c4659878
JIAJUNKK <kongjiajun040103@gmail.com>
2025-10-08 21:58:52 +0100

    fixed deprecation issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./app.scss";
+import "./app/base.scss";
 import AboutMe from "./components/aboutMe/AboutMe";
 import Contact from "./components/contact/Contact";
 import Cursor from "./components/cursor/Cursor";
@@ -9,31 +9,31 @@ import Projects from "./components/projects/Projects";
 import TechStack from "./components/techstack/TechStack";
 
 const App = () => {
-  return (
-    <div>
-      <Cursor />
-      <section id="Home">
-        <Navbar />
-        <Hero />
-        <TechStack/>
-      </section>
+    return (
+        <div>
+            <Cursor />
+            <section id="Home">
+                <Navbar />
+                <Hero />
+                <TechStack />
+            </section>
 
-      <section id="About">
-        <AboutMe />
-      </section>
+            <section id="About">
+                <AboutMe />
+            </section>
 
-      <section id="Experience">
-        <Experience />
-      </section>
+            <section id="Experience">
+                <Experience />
+            </section>
 
-      <Projects/>
-      
-      <section id="Contact">
-        <Contact />
-      </section>
+            <Projects />
 
-    </div>
-  );
+            <section id="Contact">
+                <Contact />
+            </section>
+
+        </div>
+    );
 };
 
 export default App;
